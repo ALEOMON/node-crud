6e15b5402cb9dc921ecfe9a582855a3aa34e477c
ALEOMON <95180763+ALEOMON@users.noreply.github.com>
2022-01-06 13:46:47 +0000

    remove unused declaration

--- src/engine/debug/debug_drawer.cpp
+++ src/engine/debug/debug_drawer.cpp
@@ -15,4 +15,19 @@ void DebugDrawer::render(const glm::mat4 &view_projection_matrix) {
   for (auto &line : this->line_queue) {
     positions.emplace_back(line.A.x);
     positions.emplace_back(line.A.y);
-    positions.emp
+    positions.emplace_back(line.A.z);
+    positions.emplace_back(line.B.x);
+    positions.emplace_back(line.B.y);
+    positions.emplace_back(line.B.z);
+  }
+
+  unsigned int pos_buffer;
+  glGenBuffers(1, &pos_buffer);
+  glBindBuffer(GL_ARRAY_BUFFER, pos_buffer);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(float) * positions.size(), positions.data(), GL_STATIC_DRAW);
+
+  std::vector<float> colors;
+  for (auto &line : this->line_queue) {
+    for (int i = 0; i < 2; i++) {
+      colors.emplace_back(line.color.x);
+ 

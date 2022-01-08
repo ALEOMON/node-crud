cbb729ab59332661227be6d4d09b68818d18e370
ALEOMON <95180763+ALEOMON@users.noreply.github.com>
2022-01-08 21:52:32 +0000

    aio: add aio_kernel_() interface

--- src/engine/debug/debug_drawer.hpp
+++ src/engine/debug/debug_drawer.hpp
@@ -6,4 +6,24 @@
 
 #include <glm/mat4x4.hpp>
 #include <glm/vec3.hpp>
-#in
+#include <vector>
+
+namespace open_pokemon_tcg::engine::debug {
+
+  struct Line {
+    glm::vec3 A, B;
+    glm::vec3 color;
+
+    Line(glm::vec3 A, glm::vec3 B) : A(A), B(B), color(glm::vec3(1.0f, 0.0f, 0.0f)) {}
+    Line(glm::vec3 A, glm::vec3 B, glm::vec3 color) : A(A), B(B), color(color) {}
+  };
+
+  class DebugDrawer {
+  public:
+    DebugDrawer();
+    ~DebugDrawer();
+
+    // Mutators
+    void render(const glm::mat4 &view_projection_matrix);
+    void draw_line(glm::vec3 from, glm::vec3 to);
+  

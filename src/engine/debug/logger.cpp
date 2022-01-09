455f03d38d5fbc7f488450167b425ca62a668a68
ALEOMON <95180763+ALEOMON@users.noreply.github.com>
2022-01-09 14:45:47 +0000

    Update for API changes

--- src/engine/debug/logger.cpp
+++ src/engine/debug/logger.cpp
@@ -7,4 +7,26 @@ using namespace open_pokemon_tcg::engine::debug;
 
 bool checkGLError(const char* file, int line) {
 	bool wasError = false;
-	for (GLenum glErr = glGetError(); glErr != GL_NO_ER
+	for (GLenum glErr = glGetError(); glErr != GL_NO_ERROR; glErr = glGetError()) {
+    LOG_ERROR("OpenGL error #" + std::to_string(glErr) + " in " + file + " at line: " + std::to_string(line));
+    wasError = true;
+  }
+	return wasError;
+}
+
+void Logger::set_profile(Logger::Profile profile) {
+  switch (profile) {
+  case Profile::DEBUG:
+    apply_debug_profile();
+    break;
+  case Profile::DEV:
+    apply_dev_profile();
+    break;
+  case Profile::RELEASE:
+    apply_release_profile();
+    break;
+  }
+}
+
+void Logger::apply_debug_profile() {
+  spdlog::set_

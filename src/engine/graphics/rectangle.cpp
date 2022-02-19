c4dc15874c0cb8367b81bd5f9f855ef10c9d347b
ALEOMON <95180763+ALEOMON@users.noreply.github.com>
2022-02-19 13:31:23 +0000

    Little correction

--- src/engine/graphics/rectangle.cpp
+++ src/engine/graphics/rectangle.cpp
@@ -12,4 +12,18 @@ Rectangle::Rectangle(geometry::Rectangle rectangle, const Texture *texture) : _t
   _shader = std::make_unique<Shader>("texture.vert", "texture.frag");
 
   glm::vec3 botleft  = rectangle.botleft();
-  glm::vec3 b
+  glm::vec3 botright = rectangle.botright();
+  glm::vec3 topleft  = rectangle.topleft();
+  glm::vec3 topright = rectangle.topright();
+
+  std::vector<float> positions = {
+    //	 X      Y     Z
+      botleft.x , botleft.y , botleft.z,
+      botright.x, botright.y, botright.z,
+      topright.x, topright.y, topright.z,
+      topleft.x , topleft.y , topleft.z,
+  };
+
+  unsigned int pos_buffer;
+  glGenBuffers(1, &pos_buffer); // Gen buffer object and store buffer id
+  glBindB

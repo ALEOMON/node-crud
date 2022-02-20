9e1667b4475e24cefd88974a2982c2c7fe4e5380
ALEOMON <95180763+ALEOMON@users.noreply.github.com>
2022-02-20 08:20:32 +0000

    hrtimer: Consider preemption when migrating hrtimer cpu_bases

--- src/engine/graphics/rectangle.cpp
+++ src/engine/graphics/rectangle.cpp
@@ -26,4 +26,17 @@ Rectangle::Rectangle(geometry::Rectangle rectangle, const Texture *texture) : _t
 
   unsigned int pos_buffer;
   glGenBuffers(1, &pos_buffer); // Gen buffer object and store buffer id
-  glBindB
+  glBindBuffer(GL_ARRAY_BUFFER, pos_buffer);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(float) * positions.size(), positions.data(), GL_STATIC_DRAW);
+
+  unsigned int uv_buffer;
+  if (_texture != nullptr) {
+    glGenBuffers(1, &uv_buffer);
+    glBindBuffer(GL_ARRAY_BUFFER, uv_buffer);
+    const std::vector<float> uv_coords = {
+      0.0f, 0.0f, // (u,v) for v0
+      1.0f, 0.0f, // (u,v) for v1
+      1.0f, 1.0f, // (u,v) for v2
+      0.0f, 1.0f, // (u,v) for v3
+    };
+    glBuffer

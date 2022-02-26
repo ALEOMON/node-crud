623bf95ab0e7ab00e2b16ce10ba5c0c55a23f3ec
ALEOMON <95180763+ALEOMON@users.noreply.github.com>
2022-02-26 16:31:38 +0000

    [MERGE] merge with main addons

--- src/engine/graphics/texture.cpp
+++ src/engine/graphics/texture.cpp
@@ -30,4 +30,16 @@ Texture::Texture(std::string img_path) {
 
   glGenTextures(1, &_texture);
   glBindTexture(GL_TEXTURE_2D, _texture);
-  glTexImage2D(GL_TEXTURE_2D
+  glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, w, h, 0, GL_RGBA, GL_UNSIGNED_BYTE, image);
+  free(image);
+
+  // Clamp coords
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+
+  // Texture filtering
+  glGenerateMipmap(GL_TEXTURE_2D);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+  // Max samples (EXT stands for 

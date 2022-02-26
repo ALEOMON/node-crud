05f38d390848eef9eb182104fcbfbac332178052
ALEOMON <95180763+ALEOMON@users.noreply.github.com>
2022-02-26 14:19:26 +0000

    e1000e: Remove special case for 82573/82574 ASPM L1 disablement

--- src/engine/graphics/texture.cpp
+++ src/engine/graphics/texture.cpp
@@ -18,4 +18,16 @@ Texture::Texture(std::string img_path) {
     return;
   }
 
-  stbi_set_flip_vertically_on_load
+  stbi_set_flip_vertically_on_load(true); // Match opengl
+
+  std::string res_path = boost::dll::program_location().parent_path().string() + "/res/";
+  std::string full_img_path = res_path + img_path;
+
+  int w, h, comp;
+  unsigned char* image = stbi_load(full_img_path.c_str(), &w, &h, &comp, STBI_rgb_alpha);
+  if (image == NULL)
+    LOG_ERROR("Can not load texture: " + full_img_path);
+
+  glGenTextures(1, &_texture);
+  glBindTexture(GL_TEXTURE_2D, _texture);
+  glTexImage2D(GL_TEXTURE_2D

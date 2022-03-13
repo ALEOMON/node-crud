3ea3cae24b96e0579d4a31c3c2ff63d226571578
ALEOMON <95180763+ALEOMON@users.noreply.github.com>
2022-03-13 13:57:13 +0000

    Change copy

--- src/engine/gui/window.hpp
+++ src/engine/gui/window.hpp
@@ -20,4 +20,18 @@ namespace open_pokemon_tcg::engine::gui {
     void add_on_key_callback(std::function<void(GLFWwindow*, int)> callback);
     void add_on_mouse_click_callback(std::function<void(GLFWwindow*, int, int)> callback);
     void add_on_mouse_move_callback(std::function<void(GLFWwindow*, float, float)> callback);
-    void add_on_
+    void add_on_mouse_scroll_callback(std::function<void(GLFWwindow*, float, float)> callback);
+
+    // Accessors
+    bool is_closing();
+
+  private:
+    GLFWwindow* window;
+
+    std::vector<std::function<void(GLFWwindow*, int)>> on_key_callbacks;
+    std::vector<std::function<void(GLFWwindow*, int, int)>> on_mouse_click_callbacks;
+    std::vector<std::function<void(GLFWwindow*, float, float)>> on_mouse_move_callbacks;
+    std::vector<std::function<void(GLFWwindow*, float, float)>> on_mouse_scroll_callbacks;
+  };
+
+}

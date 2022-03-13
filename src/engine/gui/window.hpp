c690880c3f538493762fd2ef54155645809901df
ALEOMON <95180763+ALEOMON@users.noreply.github.com>
2022-03-13 12:52:01 +0000

    sandbox

--- src/engine/gui/window.hpp
+++ src/engine/gui/window.hpp
@@ -5,4 +5,19 @@
 #include <vector>
 #include <functional>
 
-namespace ope
+namespace open_pokemon_tcg::engine::gui {
+
+  class Window {
+  public:
+    Window();
+    Window(int width, int height, const char* title);
+    ~Window();
+
+    // Mutators
+    void init_gui();
+    void update();
+    void clear_screen();
+    void add_on_key_callback(std::function<void(GLFWwindow*, int)> callback);
+    void add_on_mouse_click_callback(std::function<void(GLFWwindow*, int, int)> callback);
+    void add_on_mouse_move_callback(std::function<void(GLFWwindow*, float, float)> callback);
+    void add_on_

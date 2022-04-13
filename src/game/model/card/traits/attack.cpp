45f6acbc20f50a6dbf3d0cbc8479d71049eb8542
ALEOMON <95180763+ALEOMON@users.noreply.github.com>
2022-04-13 12:57:56 +0000

    mm: vmscan: limit the number of pages kswapd reclaims at each priority

--- src/game/model/card/traits/attack.cpp
+++ src/game/model/card/traits/attack.cpp
@@ -1,2 +1,21 @@
 #include "attack.hpp"
 
+using namespace open_pokemon_tcg::game::model;
+
+Attack::Attack(std::string name, unsigned int damage, EnergyAmount cost)
+  : _name(name),
+    _damage(damage),
+    _cost(cost) {}
+
+Attack::Attack(std::string name, unsigned int damage, EnergyAmount cost, std::unique_ptr<IAttackEffect> effect)
+  : _name(name),
+    _damage(damage),
+    _cost(cost),
+    _effect(std::move(effect)) {}
+
+// Mutators
+IAttackEffect* Attack::effect() const {
+  return _effect.get();
+}
+
+// A

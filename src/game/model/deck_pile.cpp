eb1396604093a5c3dd93e2b61aa78b2a365eeffa
ALEOMON <95180763+ALEOMON@users.noreply.github.com>
2022-04-16 09:01:37 +0000

    Stupid me

--- src/game/model/deck_pile.cpp
+++ src/game/model/deck_pile.cpp
@@ -2,4 +2,30 @@
 
 #include "../../engine/debug/logger.hpp"
 
-using namespace 
+using namespace open_pokemon_tcg::game::model;
+
+DeckPile::DeckPile() = default;
+
+DeckPile::DeckPile(Deck& deck) {
+  for (std::unique_ptr<ICard>& c : deck.cards)
+    _cards.push_back(std::ref(*c));
+}
+
+DeckPile::~DeckPile() = default;
+
+ICard& DeckPile::pop() {
+  ICard &card = _cards.back().get();
+  _cards.pop_back();
+  _on_pop();
+  return card;
+}
+
+void DeckPile::on_pop(std::function<void ()> callback) const {
+  _on_pop.append(callback);
+}
+
+void DeckPile::shuffle() {
+  random_shuffle(_cards.begin(), _cards.end());
+}
+
+unsigned i

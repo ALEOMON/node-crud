e019c2d06d7b1cb52a0962b17096178971a63683
ALEOMON <95180763+ALEOMON@users.noreply.github.com>
2022-04-19 14:17:20 +0000

    Automated update of perennial data files

--- src/game/model/discard_pile.hpp
+++ src/game/model/discard_pile.hpp
@@ -7,4 +7,24 @@
 #include <functional>
 #include <vector>
 
-namespace open_pokemon_tcg::game::mo
+namespace open_pokemon_tcg::game::model {
+
+  class DiscardPile {
+  public:
+    DiscardPile();
+    ~DiscardPile();
+
+    // Mutators
+    void push(ICard& card);
+
+    // Accessors
+    void on_push(std::function<void (ICard&)> callback) const;
+    unsigned int size() const;
+    const std::vector<std::reference_wrapper<ICard>> cards() const;
+
+  private:
+    mutable engine::event::CallbackList<void (ICard&)> _on_push;
+
+    std::vector<std::reference_wrapper<ICard>> _cards;
+  };
+}
---
+++ src/game/model/effects/attack_effect.hpp
@@ -0,0 +1,22 @@
+
+#pragma once
+
+#include "card_effect_target.hpp"
+#include "../card/card.hpp"
+
+#include <vector>
+
+namespace open_pokemon_tcg::game::model {
+
+  class Player;
+
+  class IAttackEffect {
+  public:
+    // Mutators
+    virtual void activate(Player &self, Player &opponent, std::vector<std::reference_wrapper<const ICard>> targets, unsigned int &damage) = 0;
+
+    // Accessors
+    virtual std::vector<CardEffectTarget> required_targets() const { return {}; }
+  };
+
+}

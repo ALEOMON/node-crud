ab017da77fd69bc73f905d83d3c96386b490918b
ALEOMON <95180763+ALEOMON@users.noreply.github.com>
2022-04-30 22:22:04 +0000

    Fix yml

--- src/game/model/effects/heal.cpp
+++ src/game/model/effects/heal.cpp
@@ -1,1 +1,16 @@
-#
+#include "heal.hpp"
+
+#include "../../../engine/debug/logger.hpp"
+
+using namespace open_pokemon_tcg::game::model;
+
+Heal::Heal(CardEffectTarget scope, unsigned int heal_amount)
+  : _scope(scope),
+    _heal_amount(heal_amount) {}
+
+Heal::~Heal() = default;
+
+void Heal::activate(Player &self, [[maybe_unused]] Player &opponent, std::vector<std::reference_wrapper<const ICard>> targets) {
+  const PokemonCard* pokemon_target = dynamic_cast<const PokemonCard*>(&targets[0].get());
+  if (pokemon_target == nullptr)
+    LOG_E

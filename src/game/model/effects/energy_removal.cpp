7f5f7db162ac1d738f3da6bef90cafba871ea9e6
ALEOMON <95180763+ALEOMON@users.noreply.github.com>
2022-04-30 20:12:43 +0000

    update-strings

--- src/game/model/effects/energy_removal.cpp
+++ src/game/model/effects/energy_removal.cpp
@@ -10,4 +10,20 @@ EnergyRemoval::EnergyRemoval(CardEffectTarget pokemon_scope)
 EnergyRemoval::~EnergyRemoval() = default;
 
 void EnergyRemoval::activate([[maybe_unused]] Player &self, Player &opponent, std::vector<std::reference_wrapper<const ICard>> targets) {
-  const PokemonCard* pokemon_target = dynamic_cast<const PokemonCard*>(
+  const PokemonCard* pokemon_target = dynamic_cast<const PokemonCard*>(&targets[0].get());
+  if (pokemon_target == nullptr)
+    LOG_ERROR("Card must be a pokemon card.");
+
+  const BasicEnergy* energy_target = dynamic_cast<const BasicEnergy*>(&targets[1].get());
+  if (energy_target == nullptr)
+    LOG_ERROR("Card must be a pokemon card.");
+
+  opponent.detach_energy_from(*pokemon_target, *energy_target);
+}
+
+std::vector<CardEffectTarget> EnergyRemoval::required_targets() const {
+  return {
+      _pokemon_scope,
+      CardEffectTarget::ENERGY_FROM_PREVIOUS_POKEMON,
+  };
+}
---
+++ src/game/model/effects/energy_removal.hpp
@@ -0,0 +1,24 @@
+
+#pragma once
+
+#include "card_effect.hpp"
+
+namespace open_pokemon_tcg::game::model {
+
+  class EnergyRemoval : public ICardEffect {
+  public:
+    EnergyRemoval(CardEffectTarget pokemon_scope);
+    ~EnergyRemoval();
+
+    // Mutators
+    void activate(Player &self, Player &opponent, std::vector<std::reference_wrapper<const ICard>> targets) override;
+
+    // Accessors
+    std::vector<CardEffectTarget> required_targets() const override;
+
+  private:
+    CardEffectTarget _pokemon_scope;
+
+  };
+
+}

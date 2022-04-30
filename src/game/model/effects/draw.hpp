cefa8e81c943458e5666eea9d301a71198d1cb13
ALEOMON <95180763+ALEOMON@users.noreply.github.com>
2022-04-30 08:24:44 +0000

    namespace fix

---
+++ src/game/model/effects/draw.hpp
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "card_effect.hpp"
+
+namespace open_pokemon_tcg::game::model {
+
+  class Draw : public ICardEffect {
+  public:
+    Draw(unsigned int amount);
+    ~Draw();
+
+    // Mutators
+    void activate(Player &self, Player &opponent, std::vector<std::reference_wrapper<const ICard>> targets) override;
+
+    // Accessors
+    bool can_activate(Player &self, Player &opponent) const override;
+
+  private:
+    unsigned int _a

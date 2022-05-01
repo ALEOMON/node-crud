a576deaea03720e62a3f467ff35f0c219fe34bff
ALEOMON <95180763+ALEOMON@users.noreply.github.com>
2022-05-01 16:18:31 +0000

    add test task

--- src/game/model/effects/switch_active.cpp
+++ src/game/model/effects/switch_active.cpp
@@ -9,4 +9,14 @@ SwitchActive::SwitchActive(CardEffectTarget scope)
 
 SwitchActive::~SwitchActive() = default;
 
-void SwitchActive::activate(Player &self, Player &opponent, std::v
+void SwitchActive::activate(Player &self, Player &opponent, std::vector<std::reference_wrapper<const ICard>> targets) {
+  const PokemonCard* pokemon_target = dynamic_cast<const PokemonCard*>(&targets[0].get());
+  if (pokemon_target == nullptr)
+    LOG_ERROR("Card must be a pokemon card.");
+
+  if (_scope == FRIENDLY_BENCH_POKEMON)
+    self.switch_active(*pokemon_target);
+  else if (_scope == ENEMY_BENCH_POKEMON)
+    opponent.switch_active(*pokemon_target);
+  else
+    LOG_ERROR("Scope is wrong. Check construction of object

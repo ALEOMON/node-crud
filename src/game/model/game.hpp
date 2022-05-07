4f3f7b27ddac09cf33696dcd4f7730d7ae869c07
ALEOMON <95180763+ALEOMON@users.noreply.github.com>
2022-05-07 08:56:25 +0000

    modular

--- src/game/model/game.hpp
+++ src/game/model/game.hpp
@@ -10,4 +10,17 @@ namespace open_pokemon_tcg::game::model {
     ~Game();
 
     // Mutators
-    void place_on_active_slot_from_hand(const ICard&
+    void place_on_active_slot_from_hand(const ICard& card);
+    void place_on_bench_from_hand(const ICard& card);
+    void place_on_bench_from_hand(const ICard& card, unsigned int slot_index);
+    void activate_trainer_card(const ICard& card, std::vector<std::reference_wrapper<const ICard>> targets);
+
+    void attach_to_active_pokemon(const ICard& card);
+    void attach_to_bench_pokemon(const ICard& card, unsigned int slot_index);
+    void evolve_pokemon();
+
+    bool attack(unsigned int attack_index);
+    void end_turn();
+
+    // Accessors
+    void on_game_over(std::fu

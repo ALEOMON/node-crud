9e28d807dbdd06cd1bb0f461c425e86670eac4f0
ALEOMON <95180763+ALEOMON@users.noreply.github.com>
2022-05-08 11:22:08 +0000

    added home page

--- src/game/model/sandbox_game.hpp
+++ src/game/model/sandbox_game.hpp
@@ -5,3 +5,19 @@
 #include "effects/card_effect.hpp"
 
 #include <memory>
+#include <string>
+#include <array>
+
+namespace open_pokemon_tcg::game::model {
+
+  class SandboxGame {
+  public:
+    SandboxGame(std::array<std::unique_ptr<Deck>, 2>& player_decks, std::array<std::string, 2> player_names);
+    ~SandboxGame();
+
+    friend class Game;
+
+    // Mutators
+    void place_on_active_slot_from_hand(const ICard& card);
+    void place_on_bench_from_hand(const ICard& card);
+    void place_on_bench_from_hand(const ICard& card, unsigned int slot_index)

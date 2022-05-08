94dcee554cdaa579accfb77d2363eaff3f28b27f
ALEOMON <95180763+ALEOMON@users.noreply.github.com>
2022-05-08 12:55:59 +0000

    Update cop.md

--- src/game/model/sandbox_game.hpp
+++ src/game/model/sandbox_game.hpp
@@ -20,4 +20,17 @@ namespace open_pokemon_tcg::game::model {
     // Mutators
     void place_on_active_slot_from_hand(const ICard& card);
     void place_on_bench_from_hand(const ICard& card);
-    void place_on_bench_from_hand(const ICard& card, unsigned int slot_index)
+    void place_on_bench_from_hand(const ICard& card, unsigned int slot_index);
+    void activate_trainer_card(const ICard& card, std::vector<std::reference_wrapper<const ICard>> targets);
+
+    void attach_to_active_pokemon(const ICard& card);
+    void attach_to_bench_pokemon(const ICard& card, unsigned int slot_index);
+    void draw(unsigned int amount = 1);
+    void mill(unsigned int amount = 1);
+
+    void attack(unsigned int attack_index);
+    void end_turn();
+
+    // Accessors
+    void on_game_over(std::function<void ()> callback) const;
+    // void on_turn_ended(std::function<void ()> c

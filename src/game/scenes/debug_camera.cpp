7fc5784803326d588bd09d5b068c2a25c1d4b7ff
ALEOMON <95180763+ALEOMON@users.noreply.github.com>
2022-05-22 06:30:52 +0000

    Added CHANGELOG

--- src/game/scenes/debug_camera.cpp
+++ src/game/scenes/debug_camera.cpp
@@ -16,4 +16,17 @@ DebugCamera::DebugCamera(engine::gui::Window* window) : camera(engine::graphics:
 DebugCamera::DebugCamera(engine::gui::Window* window, engine::geometry::Transform transform) : camera(engine::graphics::Camera(transform)), window(window) {
   this->window->add_on_key_callback(std::bind(&DebugCamera::on_key, this, std::placeholders::_1));
   this->window->add_on_mouse_move_callback(std::bind(&DebugCamera::on_cursor, this, std::placeholders::_1, std::placeholders::_2, std::placeholders::_3));
-  
+  this->window->add_on_mouse_scroll_callback(std::bind(&DebugCamera::on_scroll, this, std::placeholders::_1, std::placeholders::_2, std::placeholders::_3));
+}
+
+DebugCamera::~DebugCamera() {}
+
+// Mutators
+void DebugCamera::set_transform(engine::geometry::Transform transform) {
+  this->camera.transform = transform;
+};
+
+void DebugCamera::on_key(GLFWwindow* window) {
+  if (glfwGetKey(window, GLFW_KEY_W))
+    this->camera.move(engine::graphics::Direction::FORWARD);
+  if (glfw

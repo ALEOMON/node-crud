fa1e4aa1cc0bed8f246dcf7315641a8dad8afafa
ALEOMON <95180763+ALEOMON@users.noreply.github.com>
2022-06-03 09:35:21 +0000

    Factory

--- src/game/scenes/debug_camera.hpp
+++ src/game/scenes/debug_camera.hpp
@@ -1,3 +1,19 @@
 #pragma once
 
 #include "../../engine/graphics/camera.hpp"
+#include "../../engine/gui/window.hpp"
+
+namespace open_pokemon_tcg::game::scenes {
+
+  class DebugCamera {
+  public:
+    DebugCamera(engine::gui::Window* window);
+    DebugCamera(engine::gui::Window* window, engine::geometry::Transform transform);
+    ~DebugCamera();
+
+    // Mutators
+    void set_transform(engine::geometry::Transform transform);
+
+    // Accessors
+    engine::geometry::Transform transform() const { return this->camera.transform; };
+    glm::mat4 view_matrix(

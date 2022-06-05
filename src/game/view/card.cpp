078101992f1e387a0592a3d315bebeaf546359a7
ALEOMON <95180763+ALEOMON@users.noreply.github.com>
2022-06-05 11:03:34 +0000

    Update how-to.yml

--- src/game/view/card.cpp
+++ src/game/view/card.cpp
@@ -9,4 +9,14 @@
 #include <glm/gtx/transform.hpp>
 #include <glm/mat4x4.hpp>
 
-using namespace 
+using namespace open_pokemon_tcg;
+using namespace open_pokemon_tcg::game::view;
+
+Card::Card(const game::model::ICard &model, engine::geometry::Transform transform)
+  : _model(model),
+    transform(transform),
+    front_texture(engine::graphics::Texture("cache/cards/img/" + _model.id() + ".png")),
+    back_texture(engine::graphics::Texture("img/cardback.png")) {
+
+  engine::geometry::Rectangle rect(engine::geometry::Transform(), this->width, this->height);
+  glm::vec3 botleft  = rect.botlef

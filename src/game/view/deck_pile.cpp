560b09b016a7e17ad620f49127cec5cf3035634b
ALEOMON <95180763+ALEOMON@users.noreply.github.com>
2022-06-18 09:25:13 +0000

    Put the thingie in the thingie

--- src/game/view/deck_pile.cpp
+++ src/game/view/deck_pile.cpp
@@ -11,4 +11,22 @@ DeckPile::DeckPile(const model::DeckPile &model, engine::geometry::Transform tra
     transform(transform) {
 
   for (auto card : _model.cards())
-    _cards.push_back(Card(card, engine
+    _cards.push_back(Card(card, engine::geometry::Transform()));
+  update_card_positions();
+
+  _model.on_pop([this]() {
+    _cards.pop_back();
+    update_card_positions();
+  });
+}
+
+DeckPile::~DeckPile() {}
+
+void DeckPile::render(const glm::mat4 &view_projection_matrix, engine::graphics::Shader *shader) {
+  for (auto &c : _cards)
+    c.render(view_projection_matrix, shader);
+}
+
+void DeckPile::update_card_positions() {
+  for (unsigned int h = 0; h < _cards.size(); h++) {
+    _cards[h].transform.position = this->transform.position

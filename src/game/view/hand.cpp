ef004168646315e25d8a31838b1e391ac601a000
ALEOMON <95180763+ALEOMON@users.noreply.github.com>
2022-06-25 22:02:13 +0000

    white space fixes

--- src/game/view/hand.cpp
+++ src/game/view/hand.cpp
@@ -7,4 +7,27 @@
 using namespace open_pokemon_tcg::game::view;
 
 
-Hand::Hand(const model::Hand& model, engine::geometry::Transform transform
+Hand::Hand(const model::Hand& model, engine::geometry::Transform transform)
+  : _model(model),
+    _transform(transform) {
+
+  for (auto card : _model.cards())
+    _cards.push_back(new Card(card, engine::geometry::Transform()));
+  update();
+
+  _model.listen_on_add([this](model::ICard& card) {
+    _cards.push_back(new Card(card, engine::geometry::Transform()));
+    update();
+  });
+
+  _model.listen_on_remove([this](int index) {
+    _cards.erase(_cards.begin() + index);
+    update();
+  });
+}
+
+Hand::~Hand() {}
+
+// Mutators
+void Hand::update() {
+  int count = _cards.size();

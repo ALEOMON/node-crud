a80241fd295fdc716d3b2392308478031fa86a6b
ALEOMON <95180763+ALEOMON@users.noreply.github.com>
2022-07-03 21:56:42 +0000

    Update cf-deployment-api-versions

--- src/game/view/player.cpp
+++ src/game/view/player.cpp
@@ -12,4 +12,15 @@
 using namespace open_pokemon_tcg::game::view;
 
 Player::Player(const model::Player &model, const IPlaymat &playmat, IPlaymat::Side playmat_side)
-  : _model(mo
+  : _model(model),
+    playmat(&playmat),
+    playmat_side(playmat_side) {
+
+  _shader = new engine::graphics::Shader("simple.vert", "simple.frag");
+
+  this->deck_pile = new DeckPile(*_model.playmat().deck_pile, playmat.deck_slot(this->playmat_side).transform());
+
+  this->discard_pile = new DiscardPile(*_model.playmat().discard_pile, playmat.discard_slot(this->playmat_side).transform());
+
+  if (playmat_side == open_pokemon_tcg::game::view::IPlaymat::Side::PLAYER1)
+    this->hand = new Hand(_mo

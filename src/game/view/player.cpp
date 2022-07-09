67ab618897104a60ae046b4570413054a8e166b1
ALEOMON <95180763+ALEOMON@users.noreply.github.com>
2022-07-09 20:55:22 +0000

    fixing the bug

--- src/game/view/player.cpp
+++ src/game/view/player.cpp
@@ -23,4 +23,11 @@ Player::Player(const model::Player &model, const IPlaymat &playmat, IPlaymat::Si
   this->discard_pile = new DiscardPile(*_model.playmat().discard_pile, playmat.discard_slot(this->playmat_side).transform());
 
   if (playmat_side == open_pokemon_tcg::game::view::IPlaymat::Side::PLAYER1)
-    this->hand = new Hand(_mo
+    this->hand = new Hand(_model.hand(), engine::geometry::Transform(glm::vec3(0.0f, 0.5f, -4.75f),
+                                                 glm::vec3(0.5f * glm::half_pi<float>(), glm::pi<float>(), 0.0f)));
+  else
+    this->hand = new Hand(_model.hand(), engine::geometry::Transform(glm::vec3(0.0f, 0.5f, 4.75f),
+                                                 glm::vec3(0.5f * glm::half_pi<float>(), 0.0f, 0.0f)));
+
+  std::array<engine::geometry::Rectangle, 6> prize_slots = this->playmat->prize_slots(this->playmat_side);
+  std::arra

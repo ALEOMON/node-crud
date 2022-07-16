e27641d5228c74115524457813ec88a40056e07d
ALEOMON <95180763+ALEOMON@users.noreply.github.com>
2022-07-16 20:21:37 +0000

    18

--- src/game/view/playmat.hpp
+++ src/game/view/playmat.hpp
@@ -26,4 +26,12 @@ namespace open_pokemon_tcg::game::view {
     // Accessors
     std::array<engine::geometry::Rectangle, 16> player_slots(Side side) const;
     virtual engine::geometry::Rectangle deck_slot(Side side) const = 0;
-    virtual engine::geometry::Rectangle discard_slot(Side side) const 
+    virtual engine::geometry::Rectangle discard_slot(Side side) const = 0;
+    virtual engine::geometry::Rectangle active_slot(Side side) const = 0;
+    virtual engine::geometry::Rectangle supporter_slot(Side side) const = 0;
+    virtual engine::geometry::Rectangle stadium_slot(Side side) const = 0;
+    virtual std::array<engine::geometry::Rectangle, 5> bench_slots(Side side) const = 0;
+    virtual std::array<engine::geometry::Rectangle, 6> prize_slots(Side side) const = 0;
+
+  protected:
+    static engine::geometry::Transform mirro

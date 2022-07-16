ad9fc33adaf39669e72730ca8c5fb03964ce2b01
ALEOMON <95180763+ALEOMON@users.noreply.github.com>
2022-07-16 16:56:33 +0000

    chore: bump manifest version

--- src/game/view/playmat.hpp
+++ src/game/view/playmat.hpp
@@ -8,3 +8,22 @@
 
 #include <array>
 #include <vector>
+
+namespace open_pokemon_tcg::game::view {
+  class IPlaymat {
+  public:
+    IPlaymat();
+    virtual ~IPlaymat() = 0;
+
+    enum Side {
+      PLAYER1 = 0,
+      PLAYER2 = 1,
+    };
+
+    // Mutators
+    virtual void render(const glm::mat4 &view_projection) = 0;
+
+    // Accessors
+    std::array<engine::geometry::Rectangle, 16> player_slots(Side side) const;
+    virtual engine::geometry::Rectangle deck_slot(Side side) const = 0;
+    virtual engine::geometry::Rectangle discard_slot(Side side) const 

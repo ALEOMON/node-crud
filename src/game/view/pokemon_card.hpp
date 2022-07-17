8864a8782253594c42e28e68302e24cfb5a66445
ALEOMON <95180763+ALEOMON@users.noreply.github.com>
2022-07-17 16:09:23 +0000

    added debugging info

--- src/game/view/pokemon_card.hpp
+++ src/game/view/pokemon_card.hpp
@@ -14,4 +14,19 @@ namespace open_pokemon_tcg::game::view {
 
     // Accessors
     glm::mat4 model_matrix() const;
-    engine::geometry::Inte
+    engine::geometry::Intersection* does_intersect(engine::geometry::Ray ray) const;
+    const engine::graphics::Texture& texture() const;
+    const std::vector<Card*> energy_views() const;
+
+  private:
+    game::model::PokemonCard& _model;
+    Card& _pokemon_view;
+    std::vector<Card*> _energy_views;
+
+    void update_transforms();
+
+  public:
+    engine::geometry::Transform transform; // FIXME. Maybe a pub ref function?
+
+  };
+}
---
+++ src/game/view/prize_card_pool.cpp
@@ -0,0 +1,5 @@
+#include "prize_card_pool.hpp"
+
+#include "../../engine/debug/logger.hpp"
+
+using name

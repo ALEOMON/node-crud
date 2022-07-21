0ea22caf0f6e851a2edd0d5c60011af3c59b28a5
ALEOMON <95180763+ALEOMON@users.noreply.github.com>
2022-07-21 11:01:38 +0000

    Fix tags.

--- src/game/view/prize_card_pool.hpp
+++ src/game/view/prize_card_pool.hpp
@@ -10,4 +10,17 @@ namespace open_pokemon_tcg::game::view {
 
   class PrizeCardPool {
   public:
-    PrizeCardPool(const model::PrizeCardPool &mo
+    PrizeCardPool(const model::PrizeCardPool &model, std::array<engine::geometry::Transform, 6> prize_slots);
+    ~PrizeCardPool();
+
+    // Mutators
+    void render(const glm::mat4 &view_projection_matrix, engine::graphics::Shader *shader);
+
+  private:
+    const model::PrizeCardPool &_model;
+    std::array<engine::geometry::Transform, 6> _prize_slots;
+    std::array<std::unique_ptr<Card>, 6> _prize_cards;
+
+  };
+
+}
---
+++ vendor/eventpp/eventpp/callbacklist.h
@@ -0,0 +1,3 @@
+// eventpp library
+// Copyright (C) 2018 Wang Qi (wqking)
+// Github: https://github

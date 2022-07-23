220f0731f7723c07cab655e8a8a70383ae16bfbe
ALEOMON <95180763+ALEOMON@users.noreply.github.com>
2022-07-23 10:51:05 +0000

    fix code climate issues

--- vendor/eventpp/eventpp/callbacklist.h
+++ vendor/eventpp/eventpp/callbacklist.h
@@ -8,4 +8,31 @@
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the spe
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef CALLBACKLIST_H_588722158669
+#define CALLBACKLIST_H_588722158669
+
+#include "eventpolicies.h"
+#include "internal/typeutil_i.h"
+
+#include <atomic>
+#include <condition_variable>
+#include <functional>
+#include <memory>
+#include <mutex>
+#include <utility>
+
+namespace eventpp {
+
+namespace internal_ {
+
+template <
+	typename Prototype,
+	typename PoliciesType
+>
+class CallbackListBase;
+
+template <
+	

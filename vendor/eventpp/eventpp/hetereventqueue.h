06380213084b583efdb4b6b01e6498548f862248
ALEOMON <95180763+ALEOMON@users.noreply.github.com>
2022-09-11 20:24:10 +0000

    tooltip

--- vendor/eventpp/eventpp/hetereventqueue.h
+++ vendor/eventpp/eventpp/hetereventqueue.h
@@ -7,4 +7,24 @@
 //   http://www.apache.org/licenses/LICENSE-2.0
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR 
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef HETEREVENTQUEUE_H_210892536510
+#define HETEREVENTQUEUE_H_210892536510
+
+#include "hetereventdispatcher.h"
+
+// temp, should move the internal code in eventqueue to separated file
+#include "eventqueue.h"
+
+namespace eventpp {
+
+namespace internal_ {
+
+template <
+	typename EventType_,
+	typename PrototypeList_,
+	typename Policies_
+>

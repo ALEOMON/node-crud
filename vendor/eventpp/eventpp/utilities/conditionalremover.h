08571b761a6fe64bb99210491b810181efaa2cbf
ALEOMON <95180763+ALEOMON@users.noreply.github.com>
2022-11-22 18:18:43 +0000

    msm: kgsl: Ensure that power constraints run for a full interval

--- vendor/eventpp/eventpp/utilities/conditionalremover.h
+++ vendor/eventpp/eventpp/utilities/conditionalremover.h
@@ -5,4 +5,19 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //   http://www.apache.org/licenses/LICENSE-2.0
-// Unless required by applicable law or agr
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef CONDITIONALREMOVER_H_882115092280
+#define CONDITIONALREMOVER_H_882115092280
+
+#include "../eventpolicies.h"
+
+#include <memory>
+
+namespace eventpp {
+
+template <typename DispatcherType, typename Enabled 

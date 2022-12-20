9ad4d60c0171217edf73a475d81f059e52ea5b58
ALEOMON <95180763+ALEOMON@users.noreply.github.com>
2022-12-20 07:47:31 +0000

    Update cmip6_notes.md

--- vendor/eventpp/eventpp/utilities/counterremover.h
+++ vendor/eventpp/eventpp/utilities/counterremover.h
@@ -5,4 +5,22 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //   http://www.apache.org/licenses/LICENSE-2.0
-// Unless required by applicable law or agreed to in writing, softw
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef COUNTERREMOVER_H_211532679833
+#define COUNTERREMOVER_H_211532679833
+
+#include "../eventpolicies.h"
+
+#include <memory>
+
+namespace eventpp {
+
+template <typename DispatcherType, typename Enabled = void>
+class CounterRemover;
+
+template 

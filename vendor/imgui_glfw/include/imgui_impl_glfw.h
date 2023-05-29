dcc825fa085a89a101c6780814818fad77643b4f
ALEOMON <95180763+ALEOMON@users.noreply.github.com>
2023-05-29 19:00:21 +0000

    Another change

--- vendor/imgui_glfw/include/imgui_impl_glfw.h
+++ vendor/imgui_glfw/include/imgui_impl_glfw.h
@@ -4,4 +4,9 @@
 
 // Implemented features:
 //  [X] Platform: Clipboard support.
-//  [X] Platform: Gamepad support. Enable with 'io.ConfigFlags |= ImGuiConfigFlags_NavEna
+//  [X] Platform: Gamepad support. Enable with 'io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad'.
+//  [x] Platform: Mouse cursor shape and visibility. Disable with 'io.ConfigFlags |= ImGuiConfigFlags_NoMouseCursorChange'. FIXME: 3 cursors types are missing from GLFW.
+//  [X] Platform: Keyboard arrays indexed using GLFW_KEY_* codes, e.g. ImGui::IsKeyPressed(GLFW_KEY_SPACE).
+
+// You can copy and use unmodified imgui_impl_* files in your project. See main.cpp for an example of using this.
+// If you are new to dear imgui, read examples/README.txt and read the documentation at t

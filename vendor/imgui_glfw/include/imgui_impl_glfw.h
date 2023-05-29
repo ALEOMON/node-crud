7e2d3904bfa6e486209850898f26876d6f58b8cc
ALEOMON <95180763+ALEOMON@users.noreply.github.com>
2023-05-29 11:00:47 +0000

    Major Updates

---
+++ vendor/imgui_glfw/include/imgui_impl_glfw.h
@@ -0,0 +1,7 @@
+// dear imgui: Platform Binding for GLFW
+// This needs to be used along with a Renderer (e.g. OpenGL3, Vulkan..)
+// (Info: GLFW is a cross-platform general purpose library for handling windows, inputs, OpenGL/Vulkan graphics context creation, etc.)
+
+// Implemented features:
+//  [X] Platform: Clipboard support.
+//  [X] Platform: Gamepad support. Enable with 'io.ConfigFlags |= ImGuiConfigFlags_NavEna
